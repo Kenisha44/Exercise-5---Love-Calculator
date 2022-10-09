5644fa36364d869b5101ec0d00832c687fc335a0
Kenisha Johnson <kenishaajohnson@gmail.com>
2022-10-09 19:26:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-# 🚨 Don't change the code below 👇
+
 print("Welcome to the Love Calculator!")
 name1 = input("What is your name? \n")
 name2 = input("What is their name? \n")
-# 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
 combined_string = name1 + name2
 lower_case_string = combined_string.lower()
 
